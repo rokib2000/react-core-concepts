a70d6ca02b40cdc5e829fc953b6eda74d9be2b29
rokib2000 <rokib0w1@gmail.com>
2022-09-26 17:01:43 +0600

    User Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUser> </ExternalUser>
+    </div>
+  );
+}
+
+function ExternalUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h2> External User </h2>
+      <p>{users.length}</p>
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
